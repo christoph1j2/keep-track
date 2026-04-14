722cefe1b8e98cf89f1f0f4daea865fc7c19b0bf
christoph1j2 <ernstleska@gmail.com>
2026-04-14 18:56:40 +0200

    project init

---
+++ ui/src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-slate-100 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-blue-600 shadow-lg p-6 bg-white rounded-2xl">
+        Ahoj, Tailwind 4 funguje! 🎉
+      </h1>
+    </div>
+  )
+}
+
+export default App
