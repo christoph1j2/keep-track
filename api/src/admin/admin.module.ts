b45c1dc09f4cf8c4331c4beff23a7c30299ead32
christoph1j2 <ernstleska@gmail.com>
2026-08-30 18:03:17 +0200

    fix: add guards as providers

--- api/src/admin/admin.module.ts
+++ api/src/admin/admin.module.ts
@@ -2,9 +2,11 @@ import { Logger, Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { UsersService } from '../users/users.service';
+import { RolesGuard } from '../common/guards/roles.guard';
+import { JwtAuthGuard } from '../auth/jwt-auth.guard';
 
 @Module({
   controllers: [AdminController],
-  providers: [AdminService, UsersService, Logger],
+  providers: [AdminService, UsersService, Logger, JwtAuthGuard, RolesGuard],
 })
 export class AdminModule {}
